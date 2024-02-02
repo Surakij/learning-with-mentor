e9777b6215252313e57f771199f254fa0a1b7548
Sergey Kazhaev <surakij6@gmail.com>
2024-02-02 19:47:48 +0400

    add function compose & test

--- lesson_1_on_05.02.24/codewars_task_205.js
+++ lesson_1_on_05.02.24/codewars_task_205.js
@@ -12,10 +12,10 @@ export function formatWords(words) {
 
   if (newArr.length === 1) {
     return newArr.toString();
-  } if (newArr.length === 2) {
+  }
+  if (newArr.length === 2) {
     return newArr.join(" and ");
-  } 
-    const arr1 = newArr.slice(-2).join(" and ");
-    return `${newArr.slice(0, newArr.length - 2).join(", ")  }, ${  arr1}`;
-  
+  }
+  const arr1 = newArr.slice(-2).join(" and ");
+  return `${newArr.slice(0, newArr.length - 2).join(", ")}, ${arr1}`;
 }
